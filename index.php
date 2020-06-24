78b6cb285c27d59892fea53a0108774ced430334
Laura <lblancov@unbosque.edu.co>
2020-06-24 18:31:01 -0500

    Iconos dorados

--- index.php
+++ index.php
@@ -32,14 +32,7 @@
     </section>
 
 
-    <div class="social">
-      <ul>
-        <li><a href="https://www.facebook.com/glamista.store" target="_blank" class="icon-facebook"><img src="img/glamista/facebook.png"  width="30" height="30" alt=""></a></li>
-        <li><a href="https://www.instagram.com/glamista.store/" target="_blank" class="icon-twitter"><img src="img/glamista/instagram.png"  width="30" height="30" alt=""></a></li>
-        <li><a href="https://wa.link/x3bt1v" target="_blank" class="icon-googleplus"><img src="img/glamista/whatsapp.png"  width="30" height="30" alt=""></a></li>
-      </ul>
-    </div>
-    
+  
     <!--================ Hero Carousel start =================-->
     <section class="section-margin mt-0">
       <div class="owl-carousel owl-theme hero-carousel">
